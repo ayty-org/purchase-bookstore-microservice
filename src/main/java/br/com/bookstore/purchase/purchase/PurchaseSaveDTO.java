5c80072a8f1cc570a9e65c008a46098a381763f5
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2021-03-03 18:38:40 -0300

    feat(#3): adjusting code in entitys of Purchase

--- src/main/java/br/com/bookstore/purchase/purchase/PurchaseSaveDTO.java
+++ src/main/java/br/com/bookstore/purchase/purchase/PurchaseSaveDTO.java
@@ -5,29 +5,26 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.domain.Page;
 
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class PurchaseDTO implements Serializable {
+public class PurchaseSaveDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
 
     @NotNull
-    private ClientDTO client;
+    private String specificIdClient;
 
     @NotNull
-    private Set<BookDTO> purchasedBooks;
+    private Set<String> specificIdBooks;
 
     @NotNull
     private double amountToPay;
