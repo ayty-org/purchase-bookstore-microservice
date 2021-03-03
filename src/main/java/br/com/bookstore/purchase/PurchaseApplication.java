b482312d488a9199a5ea20cacfa7c51a4f9e3ab9
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2021-03-02 21:25:50 -0300

    feat(#1): configure project

---
+++ src/main/java/br/com/bookstore/purchase/PurchaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.bookstore.purchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseApplication.class, args);
+	}
+
+}
