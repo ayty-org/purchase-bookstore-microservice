0a8ace3f08770554021ce647c4efe20f5634b120
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2021-03-08 20:03:14 -0300

    feat(#26): create documentation in swagger

---
+++ src/main/java/br/com/bookstore/purchase/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package br.com.bookstore.purchase.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    public Docket swagger(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+    @Primary
+    @Bean
+    public SwaggerResourcesProvider swaggerResourcesProvider(
+            InMemorySwaggerResourcesProvider defaultResourcesProvider) {
+        return () -> {
+            List<SwaggerResource> resources = new ArrayList<>();
+            Collections.singletonList("api1")
+                    .forEach(resourceName -> resources.add(loadResource(resourceName)));
+            return resources;
+        };
+    }
+    private SwaggerResource loadResource(String resource) {
+        SwaggerResource wsResource = new SwaggerResource();
+        wsResource.setName(resource);
+        wsResource.setSwaggerVersion("OAS 3.0");
+        wsResource.setLocation("/docs/" + resource + "/bookstore-purchase-microservice-api.yml");
+        return wsResource;
+    }
+}
